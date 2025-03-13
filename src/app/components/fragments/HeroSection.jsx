a6b95662064ed47fdaf25fe3a521a52334de6b37
KLaZ <muhabdanulikhlas0983@gmail.com>
2025-03-13 10:30:37 +0700

    update px description hero section component

--- src/app/components/fragments/HeroSection.jsx
+++ src/app/components/fragments/HeroSection.jsx
@@ -4,7 +4,7 @@ const HeroSection = ({ title, highlight, description }) => {
       <h1 className="text-2xl bg-gradient-to-r from-white to-gray-600 bg-clip-text text-transparent">
         <span className="text-yellow-500">{highlight}</span> {title}
       </h1>
-      <p className="mt-4 text-xs leading-5 px-4 text-gray-300 max-w-2xl mx-auto ">
+      <p className="mt-4 text-xs leading-5 px-3 text-gray-300 max-w-2xl mx-auto ">
         {description}
       </p>
     </section>
