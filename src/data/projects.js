b8a4c3c4e7acd92aa99e5e228795f28436f0ec33
KLaZ <muhabdanulikhlas0983@gmail.com>
2025-03-15 12:27:40 +0700

    create component, and section project on portfolio page

---
+++ src/data/projects.js
@@ -0,0 +1,32 @@
+const projects = [
+  {
+    id: "bucketbyatiq",
+    title: "Bucket Byatiq",
+    image: "/images/projects/bucketByatiq.png",
+    link: "portfolio/bucketbyatiq",
+    hoverColor: "#193446",
+  },
+  {
+    id: "tanimas",
+    title: "Tani Mas",
+    image: "/images/projects/tanimas.png",
+    link: "portfolio/onehome",
+    hoverColor: "#1C3432",
+  },
+  {
+    id: "swanbag",
+    title: "Swanbag",
+    image: "/images/projects/swanbag.png",
+    link: "portfolio/swanbag",
+    hoverColor: "#E54614",
+  },
+  {
+    id: "onehome",
+    title: "One Home",
+    image: "/images/projects/onehome.png",
+    link: "portfolio/onehome",
+    hoverColor: "#069AA7",
+  },
+];
+
+export default projects;
