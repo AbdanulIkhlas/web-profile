95a4124afc122ef589eac7c21223d0b64ffdcf8b
KLaZ <muhabdanulikhlas0983@gmail.com>
2025-03-15 19:03:18 +0700

    implementation footer component on Service and Portfolio Page

--- src/app/components/layouts/PortfolioLayout.jsx
+++ src/app/components/layouts/PortfolioLayout.jsx
@@ -1,10 +1,12 @@
 import HeroSection from "../sections/Portfolio/HeroSection";
 import ProjectSection from "../sections/Portfolio/ProjectSection";
+import Footer from "../fragments/Footer";
 
 const PortfolioLayout = () => {
-  return <div className="w-full mb-56">
+  return <div className="w-full">
     <HeroSection />
     <ProjectSection />
+    <Footer />
   </div>;
 };
 
